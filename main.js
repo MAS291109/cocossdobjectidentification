2c511f7152b23af824899824fe2e7eeb84f5f102
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-03-08 18:06:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+img = ""; 
+
+function preload(){
+    img = loadImage('dog_cat.jpg'); 
+}
+
+function setup(){
+    canvas = createCanvas(500,350);
+    canvas.center();
+}
+
+function draw(){
+    image(img, 0, 0, 500, 350);
+    fill("#FF0000");
+    text("Dog", 40, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(35, 50, 370, 290);
+}
